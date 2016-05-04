c483cf316d1ce59afed13ffc6b378bb84e207824
Aleksandra Wozniak <aleksandra_wozniak@epam.com>
2016-05-04 14:06:18 +0200

    Calendar

---
+++ SnowballCallendar/src/main/java/pl/snowball/enums/UserProfile.java
@@ -0,0 +1,5 @@
+package pl.snowball.enums;
+
+public enum UserProfile {
+	USER, ADMIN
+}
---
+++ SnowballCallendar/src/main/webapp/resources/js/calendar.js
@@ -0,0 +1,50 @@
+$(function() {
+	for (i = startHour; i <= endHour; i++) {
+		insertRow(i);
+	}
+
+	var array = selectedCells.split(";"), i;
+	for (i = 0; i < array.length; i++) {
+		if (document.getElementById("cell_" + array[i]) != null) {
+			document.getElementById("cell_" + array[i]).className = "highlighted";
+		}
+	}
+});
+
+function insertRow(i) {
+	var table = document.getElementById('scheduleTable');
+	var row = table.insertRow(table.rows.length);
+	var cell0 = row.insertCell(0);
+	var text = document.createTextNode(i);
+	cell0.appendChild(text);
+	cell0.id = "cell_" + i + "_0";
+	for (j=1; j<=5; j++) {
+		var cell = row.insertCell(j);
+		cell.id = "cell_" + i + "_" + j;
+	}
+}
+
+$(function() {
+	var isMouseDown = false;
+	
+	$("#scheduleTable td").not(":first-child").mousedown(function(e) {
+		if (document.getElementById(e.target.id).className == "highlighted") {
+			isMouseDown = true;
+			document.getElementById(e.target.id).className = "event";
+			return false;
+		} 
+	}).mouseover(function(e) {
+		if (isMouseDown) {
+			document.getElementById(e.target.id).className = "event";
+		}
+	}).bind("selectstart", function() {
+		return false;
+	});
+	
+	$("#scheduleTable td").mouseup(function(e) {
+		if (isMouseDown) {
+			isMouseDown = false;
+			//window.location.href = userId + '-' + startCellId + '-' + stopCellId + "-scheduleAddTile.html";
+		}
+	});
+});
